636bf89c2e37955cf9b4990c5a27346ca3fe0536
Alejandro <52925451+alejandromc23@users.noreply.github.com>
2020-10-16 12:38:18 +0200

    Update web-scraping.py

--- web-scraping.py
+++ web-scraping.py
@@ -35,6 +35,11 @@ num = 1
 stage = 1
 
 # all the matches during season 18-19
+base_url = 'http://www.acb.com/partido/estadisticas/id/'
+num = 1
+jornada = 1
+folder = 'local/'
+
 for e in range(18128, 18433):
     url = base_url + str(e)
     print(url)
@@ -43,15 +48,12 @@ for e in range(18128, 18433):
     players = []
     headers = ['dorsal','nombre','minutos','puntos','t2','t2%','t3','t3%','t1','t1%','rebotes','defensivos+ofensivos','asist','robos','perdidas','contraataques','tapones_favor','tapones-contra',',mates','falta_favor','falta_contra','+/-','Valoracion']
     
-    #find all visitant teams
-    for i in soup.find_all('section', 'partido visitante'):
+    for i in soup.find_all('section', 'partido'):
         rows = i.find_all('tr')
         team = i.find('h6')
         team = team.text.replace('&nbsp;', '').replace('\xa0', ' ')
         team = team.split(' ')
         team = team[0:2]
-        
-        # find all table rows of visitant team 
         for row in rows:
             data = row.find_all('td')
             player = []
@@ -61,16 +63,21 @@ for e in range(18128, 18433):
         players = players[2:len(players)-2]
         players = [headers] + players
         
-        # 9 matches per stage so if num is higher than 9 we are in the next stage and then reestart the cycle
-        if num > 9:
+        if num > 18:
             num = 1
-            stage += 1
+            jornada += 1
             
-        # save file with file name: teamName_stageNumber.csv
-        file_name = team[0].lower() + '_' + team[1].lower() + '_' + str(stage) + '.csv'
+        file_name = folder + team[0].lower() + '_' + team[1].lower() + '_' + str(jornada) + '.csv'
+        
         np.savetxt(file_name,  
            players, 
            delimiter =", ",  
            fmt ='% s') 
+        
         num += 1
+        players = []
+        if folder == 'local/':
+            folder = 'visitant/'
+        else:
+            folder = 'local/' 
 
